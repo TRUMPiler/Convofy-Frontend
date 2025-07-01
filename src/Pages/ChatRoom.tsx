7f5cd6ef5ccea5f19f58e127da523aeb3f621e81
Naishald <2024.naishal.doshi@ves.ac.in>
2025-07-01 17:29:34 +0530

    finally the chatroom is connected to ec2 instance

--- src/Pages/ChatRoom.tsx
+++ src/Pages/ChatRoom.tsx
@@ -96,7 +96,7 @@ const ChatroomPage: React.FC = () => {
         }
 
         // Make API call to fetch chat history using the optimized backend endpoint
-        const response = await axios.get(`https://3.108.249.57:8080/api/chat/history/${chatroomId}?page=0&size=50`, {
+        const response = await axios.get(`https://api.convofy.fun/api/chat/history/${chatroomId}?page=0&size=50`, {
           headers: {
             Authorization: `Bearer ${jwtToken}`, // Include JWT for authorization
           },
